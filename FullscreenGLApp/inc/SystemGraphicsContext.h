ddc5e8871a6219f3fabce12221e07e4f266d46f8
JasonC <jason.cipriani@gmail.com>
2019-02-10 11:21:53 -0500

    Separated SystemGraphicsContext
    - broke it out into its own file to make reuse easier
    - changed glfw vs egl getprocaddress to not be a macro
    - got rid of unused fullscreenglapp.h

---
+++ FullscreenGLApp/inc/SystemGraphicsContext.h
@@ -0,0 +1,48 @@
+#ifndef SYSTEMGRAPHICSCONTEXT_H
+#define SYSTEMGRAPHICSCONTEXT_H
+
+#include <glad/glad.h> // Generated at https://glad.dav1d.de/
+
+#if USE_GLFW
+#  include <GLFW/glfw3.h>
+#else
+#  include <EGL/egl.h>
+#  include <EGL/eglext.h>
+#endif
+
+#include <ml_api.h> // MLHandle
+
+
+//-----------------------------------------------------------------------------
+/**
+ * This is basically graphics_context_t from the SimpleGLApp SDK example, with
+ * the addition of cleanup(), context(), gladLoader() and some other changes. 
+ * It just wraps windowing system stuff.
+ *
+ * This example uses the OpenGL 3.0 Compatibility Profile and uses the fixed
+ * function pipeline API to draw. You can use core profile and shaders if you
+ * want, too. Same idea re: usage of camera transformations and such. The 3.0
+ * compatibility profile is specified in the constructor, go look there.
+ */
+ //-----------------------------------------------------------------------------
+
+class SystemGraphicsContext {
+private:
+#if USE_GLFW
+    GLFWwindow *window;
+#else
+    EGLDisplay egl_display;
+    EGLContext egl_context;
+#endif
+public:
+    SystemGraphicsContext();
+    ~SystemGraphicsContext() { cleanup(); }
+    void cleanup();
+    void makeCurrent();
+    void swapBuffers();
+    void unmakeCurrent();
+    MLHandle context() const;
+    GLADloadproc loader() const;
+};
+
+#endif
--- FullscreenGLApp/src/FullscreenGLApp.cpp
+++ FullscreenGLApp/src/FullscreenGLApp.cpp
@@ -14,23 +14,11 @@
 // ---------------------------------------------------------------------
 // %BANNER_END%
 
-#include <glad/glad.h> // Generated at https://glad.dav1d.de/
-
-#if USE_GLFW
-#  include <GLFW/glfw3.h>
-#  define SYSTEM_GRAPHICS_GETPROCADDRESS ((GLADloadproc)glfwGetProcAddress)
-#else
-#  include <EGL/egl.h>
-#  include <EGL/eglext.h>
-#  define SYSTEM_GRAPHICS_GETPROCADDRESS ((GLADloadproc)eglGetProcAddress)
-#endif
-
 #include <runtime/external/glm/glm.hpp>
 #include <runtime/external/glm/gtx/quaternion.hpp>
 #include <runtime/external/glm/gtx/transform.hpp>
 #include <runtime/external/glm/gtc/type_ptr.hpp>
 
-#include "FullscreenGLApp.h"
 #include <ml_head_tracking.h>
 #include <ml_graphics.h>
 #include <ml_input.h>
@@ -40,135 +28,8 @@
 #include <ml_planes.h>
 #include <ml_privileges.h>
 
+#include "SystemGraphicsContext.h"
 
-//-----------------------------------------------------------------------------
-/**
- * This is basically graphics_context_t from the SimpleGLApp SDK example, with 
- * the addition of cleanup() and context() and some other changes. It just 
- * wraps windowing system stuff.
- *
- * This example uses the OpenGL 3.0 Compatibility Profile and uses the fixed
- * function pipeline API to draw. You can use core profile and shaders if you
- * want, too. Same idea re: usage of camera transformations and such. The 3.0
- * compatibility profile is specified in the constructor, go look there.
- */
-//-----------------------------------------------------------------------------
-
-class SystemGraphicsContext {
-private:
-#if USE_GLFW
-    GLFWwindow *window;
-#else
-    EGLDisplay egl_display;
-    EGLContext egl_context;
-#endif
-public:
-    SystemGraphicsContext();
-    ~SystemGraphicsContext() { cleanup(); }
-    void cleanup();
-    void makeCurrent();
-    void swapBuffers();
-    void unmakeCurrent();
-    MLHandle context() const;
-};
-
-#if USE_GLFW
-
-SystemGraphicsContext::SystemGraphicsContext() {
-
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
-
-    // We open a 1x1 window here -- this is not where the action happens, however;
-    // this program renders to a texture.  This is done merely to make GL happy.
-    window = glfwCreateWindow(1, 1, "SystemGraphicsContext_DummyWindow", NULL, NULL);
-
-}
-
-void SystemGraphicsContext::makeCurrent() {
-    glfwMakeContextCurrent(window);
-}
-
-void SystemGraphicsContext::unmakeCurrent() {
-}
-
-void SystemGraphicsContext::swapBuffers() {
-    glfwSwapBuffers(window);
-}
-
-void SystemGraphicsContext::cleanup() {
-    if (window) {
-        glfwDestroyWindow(window);
-        window = NULL;
-    }
-}
-
-MLHandle SystemGraphicsContext::context() const {
-    return (MLHandle)glfwGetCurrentContext();
-}
-
-#else
-
-SystemGraphicsContext::SystemGraphicsContext() {
-    egl_display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-
-    EGLint major = 4;
-    EGLint minor = 0;
-    eglInitialize(egl_display, &major, &minor);
-    eglBindAPI(EGL_OPENGL_API);
-
-    EGLint config_attribs[] = {
-      EGL_RED_SIZE, 8,            // 5/6/5 is another option
-      EGL_GREEN_SIZE, 8,
-      EGL_BLUE_SIZE, 8,
-      EGL_ALPHA_SIZE, 0,
-      EGL_DEPTH_SIZE, 24,
-      EGL_STENCIL_SIZE, 0,        // 8 works for stencil size too if you need it
-      EGL_NONE
-    };
-    EGLConfig egl_config = nullptr;
-    EGLint config_size = 0;
-    eglChooseConfig(egl_display, config_attribs, &egl_config, 1, &config_size);
-
-    EGLint context_attribs[] = {
-      EGL_CONTEXT_MAJOR_VERSION_KHR, 3,
-      EGL_CONTEXT_MINOR_VERSION_KHR, 0,
-      EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR, EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
-      EGL_NONE
-    };
-    egl_context = eglCreateContext(egl_display, egl_config, EGL_NO_CONTEXT, context_attribs);
-}
-
-void SystemGraphicsContext::makeCurrent() {
-    eglMakeCurrent(egl_display, EGL_NO_SURFACE, EGL_NO_SURFACE, egl_context);
-}
-
-void SystemGraphicsContext::unmakeCurrent() {
-    eglMakeCurrent(NULL, EGL_NO_SURFACE, EGL_NO_SURFACE, NULL);
-}
-
-void SystemGraphicsContext::swapBuffers() {
-    // buffer swapping is implicit on device (MLGraphicsEndFrame)
-}
-
-void SystemGraphicsContext::cleanup() {
-    if (egl_display) {
-        eglDestroyContext(egl_display, egl_context);
-        eglTerminate(egl_display);
-        egl_display = NULL;
-    }
-}
-
-MLHandle SystemGraphicsContext::context() const {
-    return (MLHandle)egl_context;
-}
-
-#endif
-
-
-//-----------------------------------------------------------------------------
 
 const char APP_TAG[] = "mlx.fullscreenglapp";
 
@@ -251,7 +112,7 @@ int main () {
     graphics_context.makeCurrent();
 
     // A GL context *must* be current for this to succeed!     
-    if (!gladLoadGLLoader(SYSTEM_GRAPHICS_GETPROCADDRESS)) {
+    if (!gladLoadGLLoader(graphics_context.loader())) {
         ML_LOG_TAG(Error, APP_TAG, "GL loader failed.");
         return -1;
     }
